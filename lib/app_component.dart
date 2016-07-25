5455cfa6563156cac017fc548f49651d566a590b
ofadeyi <ofadeyi@gmail.com>
2016-07-25 01:13:38 +0100

    Initial Commit: Angular Dart 'Tour of Heroes' tutorial

---
+++ lib/app_component.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2016, ofadeyi. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+
+class Hero {
+  final int id;
+  String name;
+
+  Hero(this.id, this.name);
+}
+
+@Component(selector: 'my-app', templateUrl: 'app_component.html')
+class AppComponent {
+  String title = 'Tour of Heroes';
+  Hero hero = new Hero(1, 'Windstorm');
+}
